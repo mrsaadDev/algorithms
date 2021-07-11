f4b1123a94a9281dbafa30b178094b6dde6e178a
mrsaadDev <mrsaad@gmail.com>
2021-07-11 21:38:40 +0500

    algorithm 2-mashqda 2 algoritm

---
+++ alg-2_binary-search.py
@@ -0,0 +1,29 @@
+#Linear search
+
+myList = [1,3,4,6,7,8,10,12,23,45,56,78,99]
+#
+# def linear_search(list, item):
+#     for n in range(len(list)):
+#         if list[n]==item:
+#             return n
+#     return None
+#
+# print(linear_search(myList, 12))
+
+#Binary search
+
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        mid = (low+high) // 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high= mid - 1
+        else:
+            low= mid + 1
+    return None
+
+print(binary_search(myList, 78))
